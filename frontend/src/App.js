54ef0d74027ceb4e6e0cc2c332da7fc97ad95a51
Botirjon Shokirov <botirjon@radcode.co>
2022-12-02 19:38:50 +0100

    amazona-app-beginning

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">amazona</a>
+      </header>
+      <main>list of products</main>
+    </div>
+  );
+}
+
+export default App;
